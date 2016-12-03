393f6364dcde23f01a435cba286aa2dbe660f79f
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-12-03 16:12:19 -0200

    Create channels model

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Channel(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=300)
