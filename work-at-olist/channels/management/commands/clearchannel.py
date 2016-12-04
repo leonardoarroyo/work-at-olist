852011dd5261f2f0a5a9d960d649d19afd33481e
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-12-04 14:54:12 -0200

    Refactor common command functionality to BaseChannelCommandMixin

--- work-at-olist/channels/management/commands/clearchannel.py
+++ work-at-olist/channels/management/commands/clearchannel.py
@@ -1,39 +1,11 @@
 from channels.models import Channel, Category
+from channels.management.base import BaseChannelCommandMixin
 from django.core.management.base import BaseCommand
 
 from channels.management.helpers import get_input
 
-class Command(BaseCommand):
+class Command(BaseChannelCommandMixin, BaseCommand):
     help = "Remove all categories from a channel"
-
-    def add_arguments(self, parser):
-        # Channel name used to find channel
-        parser.add_argument('channel_name', type=str)
-
-        # Run the command without user input. Used for automation.
-        parser.add_argument(
-            "--no-input",
-            action="store_true",
-            dest="no_input",
-            default=False,
-            help="Does not ask for any confirmation before executing actions"
-        )
-
-        # Test command without commiting changes
-        parser.add_argument(
-            "--dry-run",
-            action="store_true",
-            dest="dry_run",
-            default=False,
-            help="Test the command without commiting changes"
-        )
-
-    def _print(self, text, verbosity_required, file=None):
-        """ Only output a text if verbosity set to equal or higher than text verbosity level """
-        file = file or self.stdout
-        if (self.verbosity >= verbosity_required) or file==self.stderr:
-            print(text, file=file)
-
 
     def handle(self, *args, **options):
         channel_name = options['channel_name']
