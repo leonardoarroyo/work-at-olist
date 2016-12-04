3c93021807d819d50fd47f8a3ca97d51c2a6bee4
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-12-04 10:34:54 -0200

    Create Category model

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -4,3 +4,12 @@ import uuid
 class Channel(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=300)
+
+
+class Category(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=300)
+
+    # Relationships
+    channel = models.ForeignKey('channels.Channel')
+    parent = models.ForeignKey('self', blank=True, null=True)
